09c7a4b0a98c3346c273b60b87a3c88029d4890c
xoals7181-cloud <xoals7181@gmail.com>
2026-01-07 21:56:15 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+print("================================")
+print("GitHub Actions Python 실행 성공")
+print("실행 시간:", datetime.now())
+print("================================")
